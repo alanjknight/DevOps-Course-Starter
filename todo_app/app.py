d224acf76094298bb332f28257ee65b89b980fc3
Alan Knight <alanjknight@hotmail.com>
2021-06-11 15:37:44 +0100

    Added Function to add new ToDo Items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item, get_items
 from flask import Flask
 from flask import render_template
+from flask import request
 
 from todo_app.flask_config import Config
 
@@ -10,8 +11,12 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    items = get_items()
-    return render_template('index.html', items=items)
+    return render_template('index.html', items=get_items())
+
+@app.route('/', methods = ['POST'])
+def submit():
+    add_item(request.form['title'])
+    return render_template('index.html', items=get_items())
 
 
 if __name__ == '__main__':
