a496cdab916e48567a18e4b9ff185b5d51c4d3cc
Alan Knight <alanjknight@hotmail.com>
2021-06-11 17:12:46 +0100

    Made the output a table and added Actions to the tasks

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from todo_app.data.session_items import add_item, get_items
+from todo_app.data.session_items import add_item, get_items, get_item, save_item
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -7,6 +7,16 @@ from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+def _update_task(task, action):
+    if action=="start_task":
+        task["status"]="Started"
+    if action=="finish_task":
+        task["status"]="Finished"    
+    if action=="reset_task":
+        task["status"]="Not Started"    
+    save_item(task)
+
 
 
 @app.route('/')
@@ -18,6 +28,12 @@ def submit():
     add_item(request.form['title'])
     return render_template('index.html', items=get_items())
 
+
+@app.route('/update/<string:action>/<int:id>')
+def update_task(action, id):
+    task = get_item(id)
+    _update_task(task, action)
+    return render_template('index.html', items=get_items())
 
 if __name__ == '__main__':
     app.run()
