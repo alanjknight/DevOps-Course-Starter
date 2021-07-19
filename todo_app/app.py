d365c4f57554fdc986bba31ed86c2d0e37a6487c
= <=>
2021-07-19 21:57:56 +0100

    using redirects rather than render_template, but still have some ordering issues when adding/removing items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from todo_app.data.session_items import add_item, get_items, get_item, save_item, delete_item
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 from datetime import datetime
 
 from operator import itemgetter
@@ -18,32 +18,52 @@ def update_task_status(task, action):
         task["status"]="Not Started"    
     save_item(task)
 
+def is_sort_reverse(sort_col, last_sort_col):
+    sort_reverse = request.args.get('sort_reverse', session.get('sort_reverse'))
+    sort_reverse = (sort_reverse=='True')   
     
-@app.route('/', methods = ['GET'])
-def index():
-    
-    # which column are we to sort on (id passed in by default if nothing specified)
-    sort_col=request.args.get("sort_col", "id")
-
-    # is sorting to be reversed?  And convert the string value to a bool.
-    sort_reverse = request.args.get("sort_reverse", "True")
-    sort_reverse = (sort_reverse=="True")
-
-    #which column had we previosly sorted on (id passed in as default)
-    #this is used to make sure that if we sorted on the same column as before, we reverse the sort order, if its different, we revert to ascending.
-    last_sort_col=request.args.get("last_sort_col", "id")
-
     if last_sort_col == sort_col:
         sort_reverse = not sort_reverse
     else:
-        sort_reverse = False    
+        sort_reverse = False 
 
+    return sort_reverse
+
+def get_last_sort_col():
+    return request.args.get('last_sort_col', session.get('last_sort_col', 'id'))
+    
+def get_sort_col():
+    return request.args.get('sort_col', session.get('sort_col', 'id'))
+
+@app.route('/', methods = ['GET'])
+def index():
+    
+    sort_col=get_sort_col()
+    last_sort_col=get_last_sort_col()
+    sort_reverse = is_sort_reverse(sort_col, last_sort_col)
+    print (sort_col, last_sort_col, sort_reverse)
     last_sort_col=sort_col    
     
     items = get_items()
     items = sorted(items,key=itemgetter(sort_col), reverse=sort_reverse)
 
-    return render_template('index.html', items=items, last_sort_col=last_sort_col, sort_reverse=sort_reverse)
+    entered_title = session.get('entered_title')
+    entered_target_date = session.get('entered_target_date')
+    target_date_feedback = session.get('target_date_feedback')
+    title_feedback = session.get('title_feedback')
+    session['entered_title']=""
+    session['entered_target_date'] = ""
+    session['target_date_feedback'] = ""
+    session['title_feedback'] = ""
+
+
+    return render_template('index.html', items=items, 
+        last_sort_col=last_sort_col, 
+        sort_reverse=sort_reverse,
+        entered_title = entered_title,
+        entered_target_date = entered_target_date,
+        target_date_feedback = target_date_feedback,
+        title_feedback = title_feedback)
 
 @app.route('/', methods = ['POST'])
 def submit():
@@ -66,12 +86,18 @@ def submit():
         except:
             input_error = True
             target_date_feedback = "This date cannot be converted to a date in the format dd/mm/yy"
-    
+
+    session['last_sort_col'] = request.form['last_sort_col'] 
+    session['sort_reverse'] = request.form['sort_reverse']
+    session['sort_col']=request.form['last_sort_col']
+ 
     if input_error:
-        return render_template('index.html',items=get_items(),
-            entered_title=request.form['title'], entered_target_date=request.form['target_date'],
-            target_date_feedback=target_date_feedback,title_feedback=title_feedback)
-    
+        session['entered_title'] = request.form['title']
+        session['entered_target_date'] = request.form['target_date']
+        session['target_date_feedback'] = target_date_feedback
+        session['title_feedback']=title_feedback
+        return redirect(url_for('index'))
+
     add_item(request.form['title'], target_date)
     return redirect(url_for('index'))
     
